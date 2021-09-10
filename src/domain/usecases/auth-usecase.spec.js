fcc3bf3e295fbe72b59e4860b1eecb0173fb64bd
Fernando G. Lomonaco <fernando_lomonaco@outlook.com>
2021-09-10 11:53:46 -0300

    ensure AuthUseCase throws if no email is provided

---
+++ src/domain/usecases/auth-usecase.spec.js
@@ -0,0 +1,13 @@
+class AuthCase {
+  async auth (email) {
+    if (!email) { throw new Error() }
+  }
+}
+
+describe('Auth Case', () => {
+  test('Should return null if no email is provided', async () => {
+    const sut = new AuthCase()
+    const promise = sut.auth()
+    expect(promise).rejects.toThrow()
+  })
+})
