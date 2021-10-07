14721753b23b253117717c3a2277c7718c99af0e
Fernando G Lomonaco <fernando_lomonaco@outlook.com>
2021-10-07 19:42:46 -0300

    ensure encrypter and emailValidator throws if no params are provided

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -1,5 +1,6 @@
 const Encrypter = require('./encrypter')
 const bcrypt = require('bcrypt')
+const MissinParamError = require('../errors/missing-param-error')
 
 const makeSut = () => {
   return new Encrypter()
@@ -31,5 +32,11 @@ describe('Encrypter', () => {
     await sut.compare('any_value', 'hashed_value')
     expect(bcrypt.value).toBe('any_value')
     expect(bcrypt.hash).toBe('hashed_value')
+  })
+
+  test('Should throw if no params are provided', async () => {
+    const sut = new Encrypter()
+    expect(sut.compare()).rejects.toThrow(new MissinParamError('value'))
+    expect(sut.compare('any_value')).rejects.toThrow(new MissinParamError('hash'))
   })
 })
